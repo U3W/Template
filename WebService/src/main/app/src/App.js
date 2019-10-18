e4d0f29966a95fa5eb5beddef0f4d38a8fff47c6
Kacper Urbaniec <at.kacper.urbaniec@gmail.com>
2019-10-18 23:12:24 +0200

    Really last commit before eject

--- WebService/src/main/app/src/App.js
+++ WebService/src/main/app/src/App.js
@@ -1,41 +1,26 @@
-import React, { useState } from 'react';
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 
-const Loaded = ({ wasm }) => <button onClick={wasm.greet}>Click me</button>;
-
-const Unloaded = ({ loading, loadWasm }) => {
-  return loading ? (
-      <div>Loading...</div>
-  ) : (
-      <button onClick={loadWasm}>Load library</button>
-  );
-};
-
-const App = () => {
-  const [loading, setLoading] = useState(false);
-  const [wasm, setWasm] = useState(null);
-
-  const loadWasm = async () => {
-    try {
-      setLoading(true);
-      const wasm = await import("wasm-easypass");
-      setWasm(wasm);
-    } finally {
-      setLoading(false);
-    }
-  };
-
+function App() {
   return (
-      <div className="App">
-        <header className="App-header">
-          {wasm ? (
-              <Loaded wasm={wasm} />
-          ) : (
-              <Unloaded loading={loading} loadWasm={loadWasm} />
-          )}
-        </header>
-      </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
+}
 
 export default App;
